8bfe37f6b5f3936015738c7324ab9cac6cd90f82
Leon Timmermans <fawaka@gmail.com>
2011-12-11 23:51:44 +0100

    Initial implementation

--- lib/Signal/Safety.pm
+++ lib/Signal/Safety.pm
@@ -1,4 +1,5 @@
 package Signal::Safety;
+use 5.008001;
 use strict;
 use warnings;
 
@@ -7,3 +8,18 @@ use XSLoader;
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
 1;
+
+#ABSTRACT: Enable or disable safe signal handling
+
+__END__
+
+=head1 SYNOPSIS
+ {
+     local $Signal::Safety = 0;
+     do_something_scary():
+ }
+
+=head1 DESCRIPTION
+
+This module exposes perl's signal safety feature. It allows you to temporarily turn off safe signal handling in a user-friendly way.
+
